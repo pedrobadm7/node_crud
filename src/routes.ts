5f8b11165545a844b17ff89cb785c61fc52e1328
pedrobadm7 <pedrobars7a@gmail.com>
2021-12-28 18:03:32 -0300

    create route to delete

--- src/routes.ts
+++ src/routes.ts
@@ -2,18 +2,20 @@ import { Router } from 'express';
 import { CreateCategoryController } from './controllers/CreateCategoryController';
 import { DeleteCategoryController } from './controllers/DeleteCategoryController';
 import { GetAllCategoriesController } from './controllers/GetAllCategoriesController';
+import { UpdateCategoryController } from './controllers/UpdateCategoryController';
 
 const router = Router();
 
 /**
- * [X] C - CREATE
- * [X] R - READ
- * [] U - UPDATE
- * [X] D - DELETE
+ * [X] C - CREATE - POST
+ * [X] R - READ - GET
+ * [X] U - UPDATE - PUT
+ * [X] D - DELETE - DELETE
  */
 
 router.post("/categories", new CreateCategoryController().handle)
 router.get("/categories", new GetAllCategoriesController().handle)
 router.delete("/categories/:id", new DeleteCategoryController().handle)
+router.put("/categories/:id", new UpdateCategoryController().handle)
 
 export { router };
