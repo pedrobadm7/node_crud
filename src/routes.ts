b25ec2589f745b35e5f1e277971cc0e93006118e
pedrobadm7 <pedrobars7a@gmail.com>
2021-12-28 17:09:09 -0300

    create Get Categories service and controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,19 @@
 import { Router } from 'express';
 import { CreateCategoryController } from './controllers/CreateCategoryController';
+import { DeleteCategoryController } from './controllers/DeleteCategoryController';
+import { GetAllCategoriesController } from './controllers/GetAllCategoriesController';
 
 const router = Router();
 
+/**
+ * [X] C - CREATE
+ * [X] R - READ
+ * [] U - UPDATE
+ * [X] D - DELETE
+ */
+
 router.post("/categories", new CreateCategoryController().handle)
+router.get("/categories", new GetAllCategoriesController().handle)
+router.delete("/categories/:id", new DeleteCategoryController().handle)
 
 export { router };
